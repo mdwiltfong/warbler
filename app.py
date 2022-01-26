2069914cd1247c944aceb5716ce084487114f6d9
mdwiltfong <wiltfong.michael1@gmail.com>
2022-01-25 19:54:09 -0500

    logout route implemented

--- app.py
+++ app.py
@@ -20,6 +20,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = True
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "it's a secret")
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 toolbar = DebugToolbarExtension(app)
 
 connect_db(app)
@@ -114,7 +115,9 @@ def logout():
     """Handle logout of user."""
 
     # IMPLEMENT THIS
-
+    session.pop(CURR_USER_KEY);
+    flash('See Ya Later!',"primary")
+    return redirect('/login')
 
 ##############################################################################
 # General user routes:
